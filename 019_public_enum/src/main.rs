348d8009cd28304b8e8f73c3d79a204c6d476c7b
Vladimir Yavdoshenko <v.yavdoshenko@gmail.com>
2020-04-04 13:18:56 +0300

    added public enum example

---
+++ 019_public_enum/src/main.rs
@@ -0,0 +1,15 @@
+mod back_of_house {
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+}
+
+pub fn eat_at_restaurant() {
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+}
+
+fn main() {
+    eat_at_restaurant();
+}
