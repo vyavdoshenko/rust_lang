ec5fddf1a8aa6c42bca155d0fab6f6873a58f3b7
Vladimir Yavdoshenko <v.yavdoshenko@gmail.com>
2020-04-06 23:47:17 +0300

    implemented mode for collections exercises

--- 024_collections_exercises/src/main.rs
+++ 024_collections_exercises/src/main.rs
@@ -1,12 +1,14 @@
+pub use ordered_float::*;
+
 fn main() {
     let v:Vec<f64> = vec![1.0, 2.0, 5.0, 7.0, 2.0, 3.0, 9.0, 2.0, 4.0, 3.0, 2.0, 3.0, 5.0, 9.0, 10.0];
     let mean = get_mean(&v);
     let median = get_median(&v);
-    // let mode = get_mode(&v);
+    let mode = get_mode(&v);
 
     println!("Mean: {:?}", mean);
     println!("Median: {:?}", median);
-    // println!("Mode: {:?}", mode);
+    println!("Mode: {:?}", mode);
 }
 
 fn get_mean(v: &Vec<f64>) -> f64 {
@@ -26,14 +28,20 @@ fn get_median(v: &Vec<f64>) -> f64 {
     vec[vec.len() / 2]
 }
 
-// fn get_mode(v: &Vec<f64>) -> f64 {
-//     use std::collections::HashMap;
+fn get_mode(v: &Vec<f64>) -> f64 {
+    use std::collections::HashMap;
 
-//     let mut map:HashMap<OrderedFloat, u32> = HashMap::new();
+    let mut collection:HashMap<OrderedFloat<f64>, u32> = HashMap::new();
 
-//     for element in v.iter() {
-//         let count = map.entry(element).or_insert(0);
-//         *count += 1;
-//     }
+    for element in v.iter() {
+        let count = collection.entry(OrderedFloat::<f64>(*element)).or_insert(0);
+        *count += 1;
+    }
 
-// }
+    collection
+        .into_iter()
+        .max_by_key(|&(_, count)| count)
+        .map(|(val, _)| val)
+        .expect("Cannot compute the mode of zero numbers")
+        .into_inner()
+}
