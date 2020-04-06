ac0803552772ec6eea1d8ef5f758251ab47e61e4
Vladimir Yavdoshenko <v.yavdoshenko@gmail.com>
2020-04-06 23:20:23 +0300

    implemented median for collection exercises

--- 024_collections_exercises/src/main.rs
+++ 024_collections_exercises/src/main.rs
@@ -1,8 +1,12 @@
 fn main() {
     let v:Vec<f64> = vec![1.0, 2.0, 5.0, 7.0, 2.0, 3.0, 9.0, 2.0, 4.0, 3.0, 2.0, 3.0, 5.0, 9.0, 10.0];
     let mean = get_mean(&v);
+    let median = get_median(&v);
+    // let mode = get_mode(&v);
 
     println!("Mean: {:?}", mean);
+    println!("Median: {:?}", median);
+    // println!("Mode: {:?}", mode);
 }
 
 fn get_mean(v: &Vec<f64>) -> f64 {
@@ -14,3 +18,22 @@ fn get_mean(v: &Vec<f64>) -> f64 {
 
     sum / v.len() as f64
 }
+
+fn get_median(v: &Vec<f64>) -> f64 {
+    let mut vec = v.clone();
+    
+    vec.sort_by(|a, b| a.partial_cmp(b).unwrap());
+    vec[vec.len() / 2]
+}
+
+// fn get_mode(v: &Vec<f64>) -> f64 {
+//     use std::collections::HashMap;
+
+//     let mut map:HashMap<OrderedFloat, u32> = HashMap::new();
+
+//     for element in v.iter() {
+//         let count = map.entry(element).or_insert(0);
+//         *count += 1;
+//     }
+
+// }
