783c55827fffa9ca8081c67dcd5ea371f86b0487
Vladimir Yavdoshenko <v.yavdoshenko@gmail.com>
2020-04-14 22:49:16 +0300

    added propagating errors example

---
+++ 028_propagating_errors/src/main.rs
@@ -0,0 +1,45 @@
+use std::{fs, io};
+use std::fs::File;
+use std::io::Read;
+
+fn main() {
+    let _ = read_username_from_file_one_line();
+    let _ = read_username_from_file();
+    let _ = read_username_from_file_with_question_mark();
+    let _ = read_username_from_file_with_match();
+}
+
+fn read_username_from_file_one_line() -> Result<String, io::Error> {
+    fs::read_to_string("hello.txt")
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let mut s = String::new();
+
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+
+    Ok(s)
+}
+
+fn read_username_from_file_with_question_mark() -> Result<String, io::Error> {
+    let mut f = File::open("hello.txt")?;
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+fn read_username_from_file_with_match() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
