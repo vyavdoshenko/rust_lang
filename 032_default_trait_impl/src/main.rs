6785d6457f723686e0564f9aaf42df5d4fb491b3
Vladimir Yavdoshenko <v.yavdoshenko@gmail.com>
2020-04-19 14:50:35 +0300

    added return trait type

--- 032_default_trait_impl/src/main.rs
+++ 032_default_trait_impl/src/main.rs
@@ -1,14 +1,18 @@
 use default_trait_impl::Summary;
 
 fn main() {
-    let tweet = default_trait_impl::Tweet {
-        username: String::from("horse_ebooks"),
-        content: String::from("of course, as you probably already know, people"),
-        reply: false,
-        retweet: false,
-    };
+    let tweet = returns_summarizable();
 
     println!("1 new tweet: {}", tweet.summarize());
 
     default_trait_impl::notify(tweet);
+}
+
+fn returns_summarizable() -> impl Summary {
+    default_trait_impl::Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    }
 }
