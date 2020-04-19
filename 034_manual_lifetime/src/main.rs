882b2db3062518918569311d30df1ffbeafe5da7
Vladimir Yavdoshenko <v.yavdoshenko@gmail.com>
2020-04-19 17:34:07 +0300

    added generic and lifetime ref in the same function

--- 034_manual_lifetime/src/main.rs
+++ 034_manual_lifetime/src/main.rs
@@ -1,4 +1,17 @@
+use std::fmt::Display;
+
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+    where T: Display
+{
+    println!("Announcement! {}", ann);
     if x.len() > y.len() {
         x
     } else {
@@ -13,5 +26,8 @@ fn main() {
         let string2 = String::from("xyz");
         let result = longest(string1.as_str(), string2.as_str());
         println!("The longest string is {}", result);
+
+        let ann_result = longest_with_an_announcement(string1.as_str(), string2.as_str(), "announcement text");
+        println!("The longest string is {}", ann_result);
     }
 }
