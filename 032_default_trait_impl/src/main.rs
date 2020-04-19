ac9c61e99e2c65fa7c816601d824ec7e6aeb5580
Vladimir Yavdoshenko <v.yavdoshenko@gmail.com>
2020-04-19 14:37:11 +0300

    added default trait impl example

---
+++ 032_default_trait_impl/src/main.rs
@@ -0,0 +1,12 @@
+use default_trait_impl::Summary;
+
+fn main() {
+    let tweet = default_trait_impl::Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
+}
