48654ec0aca5291d75608fc7555ee4d199059af3
Vladimir Yavdoshenko <v.yavdoshenko@gmail.com>
2020-04-20 14:30:34 +0300

    switch tested functions in unit tests to private

--- 035_testing/src/lib.rs
+++ 035_testing/src/lib.rs
@@ -1,26 +1,24 @@
 #[cfg(test)]
 mod tests {
-    use crate::module;
-
     #[test]
     fn test_equality() {
-        assert_eq!(module::assert_eq_fn(), 5);
+        assert_eq!(super::assert_eq_fn(), 5);
     }
 
     #[test]
     fn test_not_equality() {
-        assert_ne!(module::assert_eq_fn(), 0);
+        assert_ne!(super::assert_eq_fn(), 0);
     }
 
     #[test]
     fn test_bool() {
-        assert!(module::assert_fn());
+        assert!(super::assert_fn());
     }
 
     #[test]
     #[should_panic]
     fn test_panic() {
-        module::panic_fn();
+        super::panic_fn();
     }
 
     #[test]
@@ -30,16 +28,15 @@ mod tests {
     }
 }
 
-pub mod module {
-    pub fn assert_fn() -> bool {
-        return true;
-    }
 
-    pub fn assert_eq_fn() -> u32 {
-        5
-    }
+pub fn assert_fn() -> bool {
+    return true;
+}
 
-    pub fn panic_fn() {
-        panic!("Test panic in unit test")
-    }
+fn assert_eq_fn() -> u32 {
+    5
+}
+
+fn panic_fn() {
+    panic!("Test panic in unit test")
 }
