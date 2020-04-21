4d40e15ca5e55ada0c1b02e93d2de445bcdb909a
Vladimir Yavdoshenko <v.yavdoshenko@gmail.com>
2020-04-21 22:49:22 +0300

    minigrep. added panic for check number of args

--- 036_minigrep/src/lib.rs
+++ 036_minigrep/src/lib.rs
@@ -5,6 +5,10 @@ pub struct Config {
 
 impl Config {
     pub fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("not enough arguments");
+        }
+        
         let query = args[1].clone();
         let filename = args[2].clone();
 
