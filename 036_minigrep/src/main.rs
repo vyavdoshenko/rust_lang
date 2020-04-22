093f7b049186288b79bbf6d9d6e021d84d067076
Vladimir Yavdoshenko <v.yavdoshenko@gmail.com>
2020-04-22 10:20:12 +0300

    minigrep. little cleanup with use

--- 036_minigrep/src/main.rs
+++ 036_minigrep/src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::process;
 
-use minigrep::*;
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -14,7 +14,7 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
 
         process::exit(1);
