903c86baf2daa6b787283ce2c09921c3b23b57d0
Vladimir Yavdoshenko <v.yavdoshenko@gmail.com>
2020-04-26 10:24:32 +0300

    cacher added for closure project

--- 037_closure/src/main.rs
+++ 037_closure/src/main.rs
@@ -11,21 +11,50 @@ fn main() {
     );
 }
 
+struct Cacher<T>
+    where T: Fn(u32) -> u32
+{
+    calculation: T,
+    value: Option<u32>,
+}
+
+impl<T> Cacher<T>
+    where T: Fn(u32) -> u32
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            },
+        }
+    }
+}
+
 fn generate_workout(intensity: u32, random_number: u32) {
-    let expensive_closure = |num| {
+    let mut expensive_result = Cacher::new(|num| {
         println!("calculating slowly...");
         thread::sleep(Duration::from_secs(2));
         num
-    };
+    });
 
     if intensity < 25 {
         println!(
             "Today, do {} pushups!",
-            expensive_closure(intensity)
+            expensive_result.value(intensity)
         );
         println!(
             "Next, do {} situps!",
-            expensive_closure(intensity)
+            expensive_result.value(intensity)
         );
     } else {
         if random_number == 3 {
@@ -33,7 +62,7 @@ fn generate_workout(intensity: u32, random_number: u32) {
         } else {
             println!(
                 "Today, run for {} minutes!",
-                expensive_closure(intensity)
+                expensive_result.value(intensity)
             );
         }
     }
