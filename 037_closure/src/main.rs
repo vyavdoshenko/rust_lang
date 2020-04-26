5e405e6bf512d45fc17038dcce70d88b39726730
Vladimir Yavdoshenko <v.yavdoshenko@gmail.com>
2020-04-26 10:20:04 +0300

    closure added

--- 037_closure/src/main.rs
+++ 037_closure/src/main.rs
@@ -11,21 +11,21 @@ fn main() {
     );
 }
 
-fn simulated_expensive_calculation(intensity: u32) -> u32 {
-    println!("calculating slowly...");
-    thread::sleep(Duration::from_secs(2));
-    intensity
-}
-
 fn generate_workout(intensity: u32, random_number: u32) {
+    let expensive_closure = |num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    };
+
     if intensity < 25 {
         println!(
             "Today, do {} pushups!",
-            simulated_expensive_calculation(intensity)
+            expensive_closure(intensity)
         );
         println!(
             "Next, do {} situps!",
-            simulated_expensive_calculation(intensity)
+            expensive_closure(intensity)
         );
     } else {
         if random_number == 3 {
@@ -33,7 +33,7 @@ fn generate_workout(intensity: u32, random_number: u32) {
         } else {
             println!(
                 "Today, run for {} minutes!",
-                simulated_expensive_calculation(intensity)
+                expensive_closure(intensity)
             );
         }
     }
