c67174850ea6d20fa555ff45cb59ac6c6dd216ba
Vladimir Yavdoshenko <v.yavdoshenko@gmail.com>
2020-05-10 22:28:31 +0300

    destructing enums matching example added

--- 057_pattern_syntax/src/main.rs
+++ 057_pattern_syntax/src/main.rs
@@ -7,6 +7,7 @@ fn main() {
     destructing_struct();
     destructing_struct_shorthand();
     destructing_struct_matching_literals();
+    destructing_enum();
 }
 
 fn matching_literals() {
@@ -90,5 +91,33 @@ fn destructing_struct_matching_literals() {
         Point { x, y: 0 } => println!("On the x axis at {}", x),
         Point { x: 0, y } => println!("On the y axis at {}", y),
         Point { x, y } => println!("On neither axis: ({}, {})", x, y),
+    }
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+fn destructing_enum() {
+    let msg = Message::ChangeColor(0, 160, 255);
+
+    match msg {
+        Message::Quit => {
+            println!("The Quit variant has no data to destructure.")
+        }
+        Message::Move { x, y } => {
+            println!(
+                "Move in the x direction {} and in the y direction {}",
+                x, y
+            );
+        }
+        Message::Write(text) => println!("Text message: {}", text),
+        Message::ChangeColor(r, g, b) => println!(
+            "Change the color to red {}, green {}, and blue {}",
+            r, g, b
+        ),
     }
 }
