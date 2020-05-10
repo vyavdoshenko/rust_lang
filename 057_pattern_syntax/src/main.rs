819667958076109ac79fccf75f9bc4512930f8b6
Vladimir Yavdoshenko <v.yavdoshenko@gmail.com>
2020-05-10 10:20:40 +0300

    matching literals example added

---
+++ 057_pattern_syntax/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    matching_literals();
+}
+
+fn matching_literals() {
+    let x = 1;
+
+    match x {
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
+}
