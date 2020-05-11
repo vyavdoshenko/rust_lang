7babe6ee19d7c3f83d09f18294e550f8855ba4aa
Vladimir Yavdoshenko <v.yavdoshenko@gmail.com>
2020-05-12 00:21:16 +0300

    unsafe project added. deref raw pointers example added

---
+++ 058_unsafe_rust/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    deref_raw_pointers();
+}
+
+fn deref_raw_pointers() {
+    let mut num = 5;
+
+    let r1 = &num as *const i32;
+    let r2 = &mut num as *mut i32;
+
+    unsafe {
+        println!("r1 is: {}", *r1);
+        println!("r2 is: {}", *r2);
+    }
+}
