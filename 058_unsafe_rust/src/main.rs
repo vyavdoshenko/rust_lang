d931dfa0b16c223e58d36ac97be4cb8bfeda196f
Vladimir Yavdoshenko <v.yavdoshenko@gmail.com>
2020-05-12 09:44:50 +0300

    wrap unsafe function with safe abstraction

--- 058_unsafe_rust/src/main.rs
+++ 058_unsafe_rust/src/main.rs
@@ -1,9 +1,13 @@
+use std::slice;
+
 fn main() {
     deref_raw_pointers();
 
     unsafe {
         dangerous();
     }
+
+    wrap_unsafe_with_safe_abstraction();
 }
 
 fn deref_raw_pointers() {
@@ -19,4 +23,29 @@ fn deref_raw_pointers() {
 }
 
 unsafe fn dangerous() {
+}
+
+fn wrap_unsafe_with_safe_abstraction() {
+    let mut v = vec![1, 2, 3, 4, 5, 6];
+
+    let r = &mut v[..];
+
+    let (a, b) = split_at_mut(r, 3);
+
+    assert_eq!(a, &mut [1, 2, 3]);
+    assert_eq!(b, &mut [4, 5, 6]);
+}
+
+fn split_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+    let len = slice.len();
+    let ptr = slice.as_mut_ptr();
+
+    assert!(mid <= len);
+
+    unsafe {
+        (
+            slice::from_raw_parts_mut(ptr, mid),
+            slice::from_raw_parts_mut(ptr.add(mid), len - mid),
+        )
+    }
 }
