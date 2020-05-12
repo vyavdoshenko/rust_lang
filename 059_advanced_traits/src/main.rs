d86907963acc74bd067d4fa4556b8a765f1b217e
Vladimir Yavdoshenko <v.yavdoshenko@gmail.com>
2020-05-12 22:24:50 +0300

    advanced traits project with examples added

---
+++ 059_advanced_traits/src/main.rs
@@ -0,0 +1,43 @@
+fn main() {
+    overload_plus_operator();
+    different_type_trait_overload();
+}
+
+use std::ops::Add;
+
+#[derive(Debug, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Add for Point {
+    type Output = Point;
+
+    fn add(self, other: Point) -> Point {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+fn overload_plus_operator() {
+    assert_eq!(
+        Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
+        Point { x: 3, y: 3 }
+    );
+}
+
+fn different_type_trait_overload() {
+    struct Millimeters(u32);
+    struct Meters(u32);
+
+    impl Add<Meters> for Millimeters {
+        type Output = Millimeters;
+
+        fn add(self, other: Meters) -> Millimeters {
+            Millimeters(self.0 + (other.0 * 1000))
+        }
+    }
+}
