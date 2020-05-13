eefc0009165d0ccc36666df387dc4e3611ddcbda
Vladimir Yavdoshenko <v.yavdoshenko@gmail.com>
2020-05-13 23:14:15 +0300

    synonym types example updated

--- 064_synonym_type/src/main.rs
+++ 064_synonym_type/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 fn main() {
     type Kilometers = i32;
 
@@ -5,4 +7,34 @@ fn main() {
     let y: Kilometers = 5;
 
     println!("x + y = {}", x + y);
+}
+
+type Thunk = Box<dyn Fn() + Send + 'static>;
+
+fn takes_long_type(f: Thunk) {
+    // --snip--
+}
+
+fn returns_long_type() -> Thunk {
+    // --snip--
+    let f: Thunk = Box::new(|| println!("hi"));
+    f
+}
+
+pub trait LongWrite {
+    fn write(&mut self, buf: &[u8]) -> std::result::Result<usize, std::io::Error>;
+    fn flush(&mut self) -> std::result::Result<(), std::io::Error>;
+
+    fn write_all(&mut self, buf: &[u8]) -> std::result::Result<(), std::io::Error>;
+    fn write_fmt(&mut self, fmt: fmt::Arguments) -> std::result::Result<(), std::io::Error>;
+}
+
+type Result<T> = std::result::Result<T, std::io::Error>;
+
+pub trait ShortWrite {
+    fn write(&mut self, buf: &[u8]) -> Result<usize>;
+    fn flush(&mut self) -> Result<()>;
+
+    fn write_all(&mut self, buf: &[u8]) -> Result<()>;
+    fn write_fmt(&mut self, fmt: fmt::Arguments) -> Result<()>;
 }
