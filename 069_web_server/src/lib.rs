457cde8fad95b1d2647db4f1ed1d678f87f064a9
Vladimir Yavdoshenko <v.yavdoshenko@gmail.com>
2020-05-16 22:16:59 +0300

    gracefull shutdown for web server thread pool added

--- 069_web_server/src/lib.rs
+++ 069_web_server/src/lib.rs
@@ -49,9 +49,21 @@ impl ThreadPool {
     }
 }
 
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
+}
+
 struct Worker {
     id: usize,
-    thread: thread::JoinHandle<()>,
+    thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
@@ -64,6 +76,6 @@ impl Worker {
             job();
         });
 
-        Worker { id, thread }
+        Worker { id, thread: Some(thread) }
     }
 }
